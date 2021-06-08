da294a78a9bdface20573bc84fd7122cf073ea9f
AndreSntos <andre.santos@bandtec.com.br>
2021-06-07 22:49:05 -0300

    subindo ultima versao p/ s.o

--- ApiMysql/src/main/java/br/com/dimension/conexao/DimensionConexao.java
+++ ApiMysql/src/main/java/br/com/dimension/conexao/DimensionConexao.java
@@ -2,9 +2,9 @@ package br.com.dimension.conexao;
 import java.sql.*;
 
 public class DimensionConexao {
-        private static final String url = "jdbc:sqlserver://dimension.database.windows.net:1433;database=Dimension;user=dimension@dimension;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
-        private static final String username = "dimension";
-        private static final String password = "#Gfgrupo10";
+        private static final String url = "dbc:sqlserver://dimensionandre.database.windows.net:1433;database=bdDimension;user=Dimension@dimensionandre;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        private static final String username = "Dimension";
+        private static final String password = "#Gf23084757810";
         
         public static Connection createConnectionToSQL() throws Exception {
             Connection connection = DriverManager.getConnection(url, username, password);
